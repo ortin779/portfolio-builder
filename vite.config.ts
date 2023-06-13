9e30066a3b1457ad080008ad189bd2b494eb5231
Naresh C <chakali.naresh@thoughtworks.com>
2023-06-13 23:34:25 +0530

    load env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+    },
+  };
 });
